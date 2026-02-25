530333fff3b2d5134c906cf951cf34879cdd7a5c
vosao <vosa.ondra@gmail.com>
2026-02-25 13:42:09 +0100

    heh

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -1,1 +1,38 @@
 # Ferenc
+
+from __future__ import print_function
+
+from robolab_turtlebot import Turtlebot, Rate, get_time
+
+bumper_names = ['LEFT', 'CENTER', 'RIGHT']
+state_names = ['RELEASED', 'PRESSED']
+
+
+def bumper_cb(msg):
+    """Bumber callback."""
+    # msg.bumper stores the id of bumper 0:LEFT, 1:CENTER, 2:RIGHT
+    bumper = bumper_names[msg.bumper]
+
+    # msg.state stores the event 0:RELEASED, 1:PRESSED
+    state = state_names[msg.state]
+
+    # Print the event
+    print('{} bumper {}'.format(bumper, state))
+
+def main():
+    turtle = Turtlebot()
+    turtle.register_bumper_event_cb(bumper_cb)
+
+    t = get_time()
+
+    rate = Rate(10)
+    while not turtle.is_shutting_down() and get_time() - t < 10:
+        turtle.cmd_velocity(0.1)
+        rate.sleep()
+
+    while not get_time() - t < 15:
+        turtle.cmd_velocity(0, 0.2)
+        rate.sleep()
+
+if __name__ == "__main__":
+    main()
