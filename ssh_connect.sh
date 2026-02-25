2811843be34920323043fe12cc5242084d963398
Zase <118931761+zase414@users.noreply.github.com>
2026-02-25 12:31:14 +0100

    ssh connect script

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -1,4 +1,11 @@
-ssh -X -t cvutusername@192.168.65.32 "mount /local 2>/dev/null; singularity shell /local/robolab_noetic_amd64.simg; source /opt/ros/lar/setup.bash"
+ssh -X -t cvut-username@192.168.65.32 "mount /local 2>/dev/null; singularity shell /local/robolab_noetic_amd64.simg"
 
-# robot backend:
+# next:
+# source /opt/ros/lar/setup.bash
+
+# 1st console: robot backend:
 # roslaunch robolab_turtlebot bringup_realsense_D435.launch
+
+# 2nd console: run python:
+# python3 /opt/ros/lar/share/robolab_turtlebot/scripts/bumper_test.py
+
