f8e70e3e0aac5c9f5cd7918135c06f2ffe51e3d9
vosao <vosa.ondra@gmail.com>
2026-03-04 13:30:38 +0100

    heh

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -4,9 +4,18 @@ from __future__ import print_function
 
 from robolab_turtlebot import Turtlebot, Rate, get_time
 
+class ButtonState(Enum):
+    PRESSED = 1
+    RELEASED = 0
+
+class Button(Enum):
+    BUTTON0 = 0
+    BUTTON1 = 1
+    BUTTON2 = 2
+
 class BumperState(Enum):
     PRESSED = 1
-    RELEASAED = 0
+    RELEASED = 0
 
 class Bumper(Enum):
     LEFT = 0
@@ -18,27 +27,37 @@ class Ferenc:
         self.turtle = Turtlebot()
         self.stop = False
 
+    def _button_cb(self, msg):
+        """Button event"""
+
+        if (msg.state == ButtonState.PRESSED) and (msg.event == Button.BUTTON0):
+            self.stop = False
+
     def _bumper_cb(self, msg):
         """Bumber callback."""
         
-        if (msg.state == BumperState.PRESSED):
+        if msg.state == BumperState.PRESSED:
             self.turtle.cmd_velocity(0, 0)
             self.stop = True
 
-        bumperstate = msg.state
+        bumper_state = msg.state
         bumper = msg.bumper
 
-        print('{} bumper {}'.format(bumper, bumperstate))
+        print('{} bumper {}'.format(bumper, bumper_state))
 
     def main(self):
         turtle = self.turtle
         turtle.register_bumper_event_cb(self._bumper_cb)
+        turtle.register_button_event_cb(self._button_cb)
 
         t = get_time()
 
         rate = Rate(10)
         while (not turtle.is_shutting_down() or self.stop) and (get_time() - t < 0):
             turtle.cmd_velocity(0.1)
+            while self.stop:
+                rate.sleep()
+
             rate.sleep()
 
         while (not turtle.is_shutting_down() or self.stop) and (get_time() - t < (2*3.14)/0.6):
