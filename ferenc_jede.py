14a1f80b82711cb59d76a60e45cd13ca2740cfb0
vosao <vosa.ondra@gmail.com>
2026-03-04 13:39:08 +0100

    heh

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -53,7 +53,7 @@ class Ferenc:
         t = get_time()
 
         rate = Rate(10)
-        while (not turtle.is_shutting_down() or self.stop) and (get_time() - t < 0):
+        while (not turtle.is_shutting_down() or self.stop) and (get_time() - t < 2):
             turtle.cmd_velocity(0.1)
             while self.stop:
                 rate.sleep()
