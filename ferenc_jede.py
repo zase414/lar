79ff52144677d7a573eb117023971acbc9ed2650
Zase <118931761+zase414@users.noreply.github.com>
2026-03-10 18:04:27 +0100

    slowera ahh

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -30,7 +30,7 @@ class Ferenc:
                 turtle.play_sound(4)
             else:
                 print("rotating")
-                turtle.cmd_velocity(0.002, 0.8)
+                turtle.cmd_velocity(0.002, 0.4)
             print(edgin)
             edgin = edging(turtle=turtle)
             rate.sleep()
