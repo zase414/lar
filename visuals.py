f52b8d06483ab4c1d4f9384d7a56ecc33ea4a2f7
vax <vaclav.kucera@trebon105.cz>
2026-03-10 16:22:38 +0100

    ehafdasfa

--- visuals.py
+++ visuals.py
@@ -1,18 +1,39 @@
 from __future__ import print_function
 from enum import IntEnum
 from robolab_turtlebot import Turtlebot, Rate, get_time
+from datetime import datetime
+
 import numpy as np
 
 import cv2
 
 def main():
     turtle = Turtlebot(rgb=True)
+
+    #takes picture and saves it on launch
+    save_img(turtle):
     while True:
         detect_balls(turtle=turtle)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
+
     cv2.destroyAllWindows()
+
+
+def save_img(turtle):
+    sleep(2)
+    turtle.get_rgb_image()
+    # get K, images, and point cloud
+    data = dict()
+    data['image_rgb'] = im
+    data['image_hsv'] = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+
+    # save data to .mat file
+    filename = datetime.today().strftime("%Y-%m-%d-%H-%M-%S") + ".mat"
+    savemat(filename, data)
+
+    print('Data saved in {}'.format(filename))
+
 
 def detect_balls(turtle):
         HUE_SIZE = 179
@@ -31,9 +52,12 @@ def detect_balls(turtle):
         v = hsv[:, :, 2]
 
         mask = (
-            (np.minimum(np.abs(h - HUE_REF), 180 - np.abs(h - HUE_REF)) < HUE_MAX) &
-            (s > SAT_MIN) &
-            (v > VALUE_MIN))
+            (np.minimum(np.abs(h - HUE_REF), 180 - np.abs(h - HUE_REF)) < HUE_MAX)
+#            &
+#            (s > SAT_MIN)
+#            &
+#            (v > VALUE_MIN)
+            )
 
         filtered = im.copy()
         filtered[~mask] = 0
