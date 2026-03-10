4890f067926afbe4d3ece676bb2ce96544a63c20
vosao <vosa.ondra@gmail.com>
2026-03-10 16:13:15 +0100

    heh

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -15,7 +15,7 @@ class Ferenc:
 
     def main(self):
         turtle = self.turtle
-        
+
         turtle.register_bumper_event_cb(lambda msge : callback_bumper_stop(self, msge))
         turtle.register_button_event_cb(lambda msge : callback_button0_resume(self, msge))
 
@@ -25,7 +25,7 @@ class Ferenc:
         while (not turtle.is_shutting_down()) and (get_time() - t < 3):
             if self.stop:
                 turtle.cmd_velocity(0, 0)
-                turtle.play_sound(1)
+                turtle.play_sound(2)
             else:
                 turtle.cmd_velocity(0.1)
             rate.sleep()
@@ -33,7 +33,7 @@ class Ferenc:
         while (not turtle.is_shutting_down()) and (get_time() - t < 15):
             if self.stop:
                 turtle.cmd_velocity(0, 0)
-                turtle.play_sound(1)
+                turtle.play_sound(2)
             else:
                 turtle.cmd_velocity(0.05, 0.15)
             rate.sleep()
