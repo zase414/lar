f48b114c48e7e4d14937b6c8175b336e4d3042ce
vosao <vosa.ondra@gmail.com>
2026-03-17 14:39:54 +0100

    depth update

--- image_proccesing.py
+++ image_proccesing.py
@@ -1,8 +1,13 @@
 from __future__ import print_function
+
+from numpy.ma.core import negative
+
 from robolab_turtlebot import Turtlebot, Rate, get_time, sleep
 from scipy.io import savemat
 
 import numpy as np
+
+ERROR = -100
 
 import cv2
 
@@ -18,7 +23,7 @@ def space_infront(turtle) -> bool:
     mask = pc[:, :, 1] < 0.25
 
     # mask point too far
-    mask = np.logical_and(mask, pc[:, :, 2] < 6.0)
+    mask = np.logical_and(mask, pc[:, :, 2] < 3.5)
 
     mask = np.logical_and(mask, pc[:, :, 1] > -0.25)
     data = np.sort(pc[:, :, 2][mask])
@@ -31,13 +36,37 @@ def space_infront(turtle) -> bool:
 
     return False
 
-def get_depth(turtle, center_x, center_y) -> float:
+def get_depth(turtle, center_x, center_y, radius) -> float:
     """Gets depth of center of detected object."""
     pc = turtle.get_point_cloud()
+    max_x = 639
+    max_y = 479
+    depth = float
     if pc is None:
         print('No point cloud')
-    # předělat aby vyprůměroval body středu a jeho osmiokolí
-    depth = float(pc[center_x, center_y, 2])
+
+    # předělat aby vyprůměroval body středu okolo středu
+    if radius < 10:
+        close_radius = 3
+        val_num = (close_radius+1)^2
+
+    elif radius < 2:
+        return ERROR
+
+    else:
+        close_radius = 3
+        val_num = (close_radius + 1) ^ 2
+
+    for i in range(-close_radius, close_radius, 1):
+        for j in range(-close_radius, close_radius, 1):
+            if ((center_x + i > 0) and (center_y + j > 0)
+                    and (center_x + i < max_x) and (center_y + j < max_y)):
+                depth += float(pc[center_x+i][center_y+j])
+            else:
+                depth += 0
+                val_num -= 1
+    depth = depth/val_num
+
 
     print(depth)
     return depth
