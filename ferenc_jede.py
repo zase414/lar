04ebb11c802826ff4162510c8a3df1638a0ab48b
vosao <vosa.ondra@gmail.com>
2026-03-24 19:11:00 +0100

    1. uloha testing

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -227,8 +227,8 @@ class Ferenc:
         cur_coords = turtle.get_odometry()
 
         # thresholds fo accurate enough stopping in given points
-        dist_thresh = 0.03
-        angle_thresh = 0.015
+        dist_thresh = 0.034
+        angle_thresh = 0.018
         angle_is_close_thresh = 0.065
 
         # current location and distance from goal point
