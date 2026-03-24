9c522607daf1954fb1dbd1e29b121ed11d11e628
vosao <vosa.ondra@gmail.com>
2026-03-24 14:12:51 +0100

    Ježdění kolem míčku test

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -212,7 +212,7 @@ class Ferenc:
     def go_ptp(self, point, rate) -> None:
         turtle = self.turtle
         cur_coords = turtle.get_odometry()
-        dist_thresh = 0.08
+        dist_thresh = 0.07
         angle_thresh = 0.015
         angle_is_close_thresh = 0.07
         x = point[0] - cur_coords[0]
@@ -229,7 +229,7 @@ class Ferenc:
                 turtle.cmd_velocity(0, 0)
                 turtle.play_sound(4)
             elif abs(angle_diff) < angle_is_close_thresh:
-                turtle.cmd_velocity(0, -0.2)
+                turtle.cmd_velocity(0, -0.15)
             else:
                 turtle.cmd_velocity(0, -0.42)
 
@@ -255,20 +255,20 @@ class Ferenc:
             rate.sleep()
 
         # while ferenc is not rotated at the calculated angle -> rotate
-        angle_diff = self.normalize_angle((point[2] + 0.02) - cur_coords[2])  # little over-rotation so it can spin only in one direction
+        angle_diff = self.normalize_angle((point[2] + 0.03) - cur_coords[2])  # little over-rotation so it can spin only in one direction
         while (not turtle.is_shutting_down()) and (abs(angle_diff) > angle_thresh):
             if self.stop:
                 turtle.cmd_velocity(0, 0)
                 turtle.play_sound(4)
 
             elif abs(angle_diff) < angle_is_close_thresh:
-                turtle.cmd_velocity(0, -0.2)
+                turtle.cmd_velocity(0, 0.15)
 
             else:
                 turtle.cmd_velocity(0, 0.42)
 
             cur_coords = turtle.get_odometry()
-            angle_diff = self.normalize_angle((point[2]+0.02) - cur_coords[2])   # little over-rotation so it can spin only in one direction
+            angle_diff = self.normalize_angle((point[2]+0.03) - cur_coords[2])   # little over-rotation so it can spin only in one direction
 
             rate.sleep()
 
