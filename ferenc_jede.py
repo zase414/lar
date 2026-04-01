07beebb0a05b071089397842a1526d77599d5b14
vosao <vosa.ondra@gmail.com>
2026-04-01 11:36:10 +0200

    Lazení objíždění

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -367,7 +367,7 @@ class Ferenc:
         """Simple P regulated rotating to wanted angle"""
         turtle = self.turtle
         max_speed = 0.7
-        Kp = 1
+        Kp = 1.75
         ang_vel = Kp * angle_diff
         ang_vel = max(min(ang_vel, max_speed), -max_speed)   # limit max speed
         turtle.cmd_velocity(0, ang_vel)
