61d88110a2f8e102959ae7c63067cc29945c4f2c
vosao <vosa.ondra@gmail.com>
2026-04-08 12:45:12 +0200

    Úprava go_forward fce

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -184,7 +184,7 @@ class Ferenc:
     def drive_around_ball(self, rate) -> None:
         """When close enough to the ball drive around it from point to point of calculated hexagon"""
         turtle = self.turtle
-        wanted_distance = 0.285  # 28.5 cm before ball stop
+        wanted_distance = 0.275  # 27.5 cm before ball stop
         rate.sleep()
         rate.sleep()
 
@@ -258,8 +258,8 @@ class Ferenc:
         cur_coords = turtle.get_odometry()
 
         # thresholds fo accurate enough stopping in given points
-        dist_thresh = 0.026
-        angle_thresh = 0.014
+        dist_thresh = 0.024
+        angle_thresh = 0.0125
 
         # current location and distance from goal point
         x = point[0] - cur_coords[0]
