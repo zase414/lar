69a766e04a592f34b0be6e2f7420e052a78d2c8d
vosao <vosa.ondra@gmail.com>
2026-04-29 10:42:46 +0200

    Vyjíždění z garáže - úprava

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -9,7 +9,7 @@ from typing import Optional, Tuple, List
 from callbacks import callback_bumper_stop, callback_button0_resume
 from visuals import detect_ball, space_infront, get_depth
 
-BALL_DISTANCE_TO_SKIP_EXIT = 0.7
+BALL_DISTANCE_TO_SKIP_EXIT = 0.75
 EXIT_GARAGE_DURATION = 2.67
 
 
