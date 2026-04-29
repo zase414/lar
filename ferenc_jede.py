099ba95e95f31e5469f622ed4dd5bbbf50ca0887
vosao <vosa.ondra@gmail.com>
2026-04-29 13:39:47 +0200

    Dálka

--- ferenc_jede.py
+++ ferenc_jede.py
@@ -538,8 +538,8 @@ class Ferenc:
             print("calculated final angle", angle)
             print("instead of angle", point[2])
 
-            #self.rotate_to_angle(angle, rate, point_of_return)
-            self.rotate_to_angle(point[2], rate, point_of_return)
+            self.rotate_to_angle(angle, rate, point_of_return)
+            #self.rotate_to_angle(point[2], rate, point_of_return)
 
         self._stop_and_wait(rate)
 
